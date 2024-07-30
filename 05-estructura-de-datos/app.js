0e49cff7a7c49548e1a1e30537db4872279c9078
stefwebsite <hurtstefy@gmail.com>
2024-07-30 17:06:49 -0500

    basedatos

---
+++ 05-estructura-de-datos/app.js
@@ -0,0 +1,20 @@
+const container = document.querySelector('.container')
+
+peliculas.forEach(element => {
+let card = document.createElement('div')
+
+card.innerHTML =
+`   <div class="card">
+        <img src='${element.image}' alt="Imagen de la tarjeta" class="card-imagen">
+        <div class="card-contenido">
+            <h2 class="card-titulo">${element.nombre}</h2>
+            <p class="card-descripcion">${element.year}</p>
+            <p class="card-descripcion">${element.direccion}</p>
+            <p class="card-descripcion">${element.guionistas}</p>
+            <p class="card-descripcion">${element.genero}</p>
+            <a href="${element.link}" class="btn">ver</a>
+        </div>
+    </div>
+`
+container.appendChild(card);
+})
